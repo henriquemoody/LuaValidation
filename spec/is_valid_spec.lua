d0d1baec9ea2c00c35b88ff1c9b8008b90e130a0
Henrique Moody <henriquemoody@gmail.com>
2015-09-14 18:36:23 -0300

    Create tests for "is_valid()" calls

---
+++ spec/is_valid_spec.lua
@@ -0,0 +1,39 @@
+local v = require("main")
+
+describe("Validation of is_valid() calls", function()
+  it("Must validate valid chains", function()
+    for index, chain in pairs({
+        v.dummy(true),
+        v.dummy(true).dummy,
+        v.dummy(true).dummy(true).dummy(true),
+      }) do
+      assert.True(chain:is_valid("whatever"), "Failed at chain #" .. index)
+    end
+  end)
+
+  it("Must invalidate invalid chains", function()
+    for index, chain in pairs({
+      v.dummy(false),
+      v.dummy(false).dummy(false),
+      v.dummy(false).dummy(true),
+      v.dummy(true).dummy(false),
+      v.dummy(false).dummy(false).dummy(false),
+      v.dummy(false).dummy(false).dummy(true),
+      v.dummy(false).dummy(true).dummy(true),
+      v.dummy(true).dummy(true).dummy(false),
+      v.dummy(true).dummy(false).dummy(false),
+    }) do
+      assert.False(chain:is_valid("whatever"), "Failed at chain #" .. index)
+    end
+  end)
+
+  it("Should not keep constraints in chains", function()
+    assert.False(v.dummy(false):is_valid("whatever"))
+    assert.True(v.dummy(true):is_valid("whatever"))
+  end)
+
+  it("Must validate chains with never constraints", function()
+    assert.False(v.never(v.dummy(true)):is_valid("whatever"))
+    assert.True(v.never(v.dummy(false)):is_valid("whatever"))
+  end)
+end)
